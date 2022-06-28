4c0a160c902955d85da6ac4e62d017a519871ff3
Dariusz Okonski <darek200180@gmail.com>
2022-06-28 07:54:42 +0100

    make cards match

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,18 @@ import "./App.css";
 import SingleCard from "./components/SingleCard";
 
 const cardImages = [
-  { src: "/img/helmet-1.png" },
-  { src: "/img/potion-1.png" },
-  { src: "/img/ring-1.png" },
-  { src: "/img/scroll-1.png" },
-  { src: "/img/shield-1.png" },
-  { src: "/img/sword-1.png" },
+  { src: "/img/helmet-1.png", matched: false },
+  { src: "/img/potion-1.png", matched: false },
+  { src: "/img/ring-1.png", matched: false },
+  { src: "/img/scroll-1.png", matched: false },
+  { src: "/img/shield-1.png", matched: false },
+  { src: "/img/sword-1.png", matched: false },
 ];
 
 export interface Card {
   id: number;
   src: string;
+  matched: boolean;
 }
 
 function App() {
@@ -41,14 +42,23 @@ function App() {
   useEffect(() => {
     if (choiceOne && choiceTwo) {
       if (choiceOne.src === choiceTwo.src) {
-        console.log("those cards match");
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
         resetTurn();
       } else {
-        console.log("those cards do not match");
         resetTurn();
       }
     }
   }, [choiceOne, choiceTwo]);
+
+  console.log({ cards });
 
   // reset choices & increase turn
   const resetTurn = () => {
